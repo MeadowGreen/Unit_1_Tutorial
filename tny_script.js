81afe79ab5f77137a755f44331ec6afd26f76cb2
Meadow Green <mgreen161@west-mec.org>
2020-01-08 15:25:43 -0700

    made clock count down

--- tny_script.js
+++ tny_script.js
@@ -10,29 +10,60 @@
    Date:   1-6-19
 
 */
+runClock();
+
+function runClock(){
+
+var currentYear=new Date();
+
+var dateStr=currentYear.toDateString();          // dateStr= 1/7/2020
+var timeStr=currentYear.toLocaleTimeString();    //timeStr= 1:54:23 pm
 
 //Display the current date and time
-document.getElementById("dateNow").innerHTML="mm/dd/yyyy <br/> hh:mm:ss" ;
+document.getElementById("dateNow").innerHTML= dateStr + "<br/>" + timeStr ;
 
-//Display the time left until New Year's Eve
-document.getElementById("days").textContent="DD";
-document.getElementById("hrs").textContent="HH";
-document.getElementById("mins").textContent="MM";
-document.getElementById("secs").textContent="SS";
+var newYear= new Date("January 1, 2021");
+var nextYear= currentYear.getFullYear()+1;
+newYear.setFullYear(nextYear);
 
-var date=new Date("January 7, 2020 13:54:23");
+var daysLeft=(newYear - currentYear)/(1000*60*60*24);
+var hrsLeft=(daysLeft - Math.floor(daysLeft))*24;
+var minsLeft= (hrsLeft - Math.floor(hrsLeft))*60;
+var secsLeft= (minsLeft - Math.floor(minsLeft))*60;
 
-var dateStr=date.toDateString();          // dateStr= 1/7/2020
-var timeStr=date.toLocaleTimeString();    //timeStr= 1:54:23 pm
 
-document.getElementById("dateNow").innerHTML=dateStr + "<br/>" + timeStr;
+document.getElementById("days").textContent=Math.floor(daysLeft);
+document.getElementById("hrs").textContent= Math.floor(hrsLeft);
+document.getElementById("mins").textContent=Math.floor(minsLeft);
+document.getElementById("secs").textContent=Math.ceil(secsLeft);
+}
 
-var daysLeft=365 - date.getDate();
-var hrsLeft= 24 - date.getHours();
-var minsLeft= 60 - date.getMinutes();
-var secsLeft= 60 - date.getSeconds();
 
-document.getElementById("days").textContent=daysLeft;
-document.getElementById("hrs").textContent=hrsLeft;
-document.getElementById("mins").textContent=minsLeft;
-document.getElementById("secs").textContent=secsLeft;
+
+
+function calcArea(length, width){
+   var area= length*width;
+   return area;
+}
+var squareArea= calcArea(6,6);
+
+console.log(squareArea);
+
+
+
+function circleArea(radius){
+   var area= Math.pow(radius, 2) + "pi";
+   return area;
+}
+ var circArea= circleArea(4);
+  console.log(circArea);
+
+
+
+
+function distance(x1, y1, x2, y2){
+   var dist= Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
+   return dist;
+}
+var distanceVar= distance(1,1,4,5);
+console.log(distanceVar);
